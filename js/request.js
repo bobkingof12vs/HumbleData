53f2ac934b82703946b20d0d6b75946eb136574c
Jacob <chisholm.jacob@gmail.com>
2016-11-06 22:45:21 -0700

    ip change from: 184.101.44.35 to: 71.209.182.140

--- js/request.js
+++ js/request.js
@@ -1,7 +1,7 @@
 req = new(function(){
 
-  this.urlAPI = 'http://184.101.44.35/hmbldt/HumbleData/api.php';
-  this.urlHTML = 'http://184.101.44.35/hmbldt/HumbleData/getPage.php';
+  this.urlAPI = 'http://71.209.182.140/hmbldt/HumbleData/api.php';
+  this.urlHTML = 'http://71.209.182.140/hmbldt/HumbleData/getPage.php';
 
   this.GET = function(type, callback, params){
     var p = (params == undefined ? {} : params);
